7252720ef40977409351d889b8e47590a963abd0
zorow888 <mohamedamine.mihit1@gmail.com>
2025-04-24 14:23:22 +0200

    Delete Gestion document- en cours directory

--- Gestion document- en cours/js/onglets/descriptif.js
+++
@@ -1,7 +0,0 @@
-
-export function afficherDescriptif(data) {
-  document.getElementById("nom").textContent = data.nom;
-  document.getElementById("mes").textContent = data.mise_en_service;
-  document.getElementById("portefeuille").textContent = data.portefeuille;
-  document.getElementById("puissance").textContent = data.puissance_kwc;
-}
